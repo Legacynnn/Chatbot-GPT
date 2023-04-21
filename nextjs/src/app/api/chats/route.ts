0be42ae2985f461c37c414534419e49005349fb1
Legacynnn <danmelomour@gmail.com>
2023-04-21 17:04:07 -0300

    Route (erro message)

--- nextjs/src/app/api/chats/route.ts
+++ nextjs/src/app/api/chats/route.ts
@@ -2,9 +2,26 @@ import { prisma } from '@/app/prisma/prisma'
 import { NextRequest, NextResponse } from 'next/server'
 
 export async function POST(request: NextRequest) {
-  const chatCreated = prisma.chat.create({
-    data: {},
+  const body = request.json()
+  const chatCreated = await prisma.chat.create({
+    data: {
+      messages: {
+        create: {
+          content: body.message,
+        },
+      },
+    },
   })
 
   return NextResponse.json(chatCreated)
+}
+
+export async function GET(request: NextRequest) {
+  const chats = await prisma.chat.findMany({
+    orderBy: {
+      created_at: 'desc',
+    },
+  })
+
+  return NextResponse.json(chats)
 }
