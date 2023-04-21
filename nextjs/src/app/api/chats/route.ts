4c95bbbc588dea34dbb4d2d7fbbe53fa0d2cdf91
Legacynnn <danmelomour@gmail.com>
2023-04-21 04:18:01 -0300

    Setup prisma

--- nextjs/src/app/api/chats/route.ts
+++ nextjs/src/app/api/chats/route.ts
@@ -2,6 +2,5 @@ import { NextRequest, NextResponse } from 'next/server'
 
 export async function POST(request: NextRequest) {
   const body = await request.json()
-
   return NextResponse.json({ body })
 }
