47afba40e3f6adf7759381d258e2b09064144e61
Legacynnn <danmelomour@gmail.com>
2023-04-21 19:45:42 -0300

    🛠️: Working on chat-service class

--- nextjs/src/grpc/client.ts
+++ nextjs/src/grpc/client.ts
@@ -12,6 +12,6 @@ const proto = grpc.loadPackageDefinition(
 ) as unknown as ProtoGrpcType
 
 export const chatClient = new proto.pb.ChatService(
-  'localhost:50051',
+  'host.docker.internal:50052',
   grpc.credentials.createInsecure(),
 )
