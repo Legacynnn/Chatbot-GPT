0db5c014dd87df9e24ad21dcd4f4e8e8fdd1780c
Legacynnn <danmelomour@gmail.com>
2023-04-22 02:03:59 -0300

    🔧: Working in chat/:id client request

--- nextjs/src/app/page.tsx
+++ nextjs/src/app/page.tsx
@@ -1,13 +1,39 @@
+'use client'
+
+import useSWR from 'swr'
+import { fetcher } from '@/http/http'
+import { Chat, Message } from '@prisma/client'
+import { useRouter } from 'next/navigation'
+
+type ChatWithFirstMessage = Chat & {
+  message: [Message]
+}
+
 export default function Home() {
+  const router = useRouter()
+  const { data: chats } = useSWR<ChatWithFirstMessage[]>('chats', fetcher, {
+    fallbackData: [],
+  })
+
+  const { data: messages } = useSWR<ChatWithFirstMessage[]>(
+    `chats//messages`,
+    fetcher,
+    {
+      fallbackData: [],
+    },
+  )
+
   return (
     <div className="flex gap-5">
       <div className="flex flex-col">
         Barra lateral
         <button type="button">New Chat</button>
         <ul>
-          <li>Chat 1</li>
-          <li>Chat 1</li>
-          <li>Chat 1</li>
+          {chats!.map((chat, key) => (
+            <li key={key} onClick={() => router.push(`/?id=${chat.id}`)}>
+              {chat.message[0].content}
+            </li>
+          ))}
         </ul>
         chats
       </div>
