d36871175dafe8f4cd9ad54e7cb3e0b8187f989a
Legacynnn <danmelomour@gmail.com>
2023-04-22 01:30:18 -0300

    🔧: Working on http.ts and add the framer

--- nextjs/src/app/page.tsx
+++ nextjs/src/app/page.tsx
@@ -1,5 +1,25 @@
 export default function Home() {
   return (
-    
+    <div className="flex gap-5">
+      <div className="flex flex-col">
+        Barra lateral
+        <button type="button">New Chat</button>
+        <ul>
+          <li>Chat 1</li>
+          <li>Chat 1</li>
+          <li>Chat 1</li>
+        </ul>
+        chats
+      </div>
+      <div>
+        Centro
+        <ul>
+          <li>Mensagens</li>
+        </ul>
+        <form action="">
+          <textarea placeholder="Digite sua pergunta"></textarea>
+        </form>
+      </div>
+    </div>
   )
 }
