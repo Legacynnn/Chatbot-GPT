1f53a855bb55c69c7200b764d942c7d5e07b53f7
Legacynnn <danmelomour@gmail.com>
2023-04-21 22:15:32 -0300

    🌍 route.ts event base fix

--- nextjs/src/app/api/messages/[messageId]/events/route.ts
+++ nextjs/src/app/api/messages/[messageId]/events/route.ts
@@ -1,7 +1,25 @@
+import { prisma } from '@/app/prisma/prisma'
 import { NextRequest } from 'next/server'
 export async function GET(
   request: NextRequest,
   { params }: { params: { chatId: string } },
 ) {
-  
+  const message = prisma.message.findUniqueOrThrow({
+    where: {
+      id: params.chatId,
+    },
+  })
+
+  const transformStream = new TransformStream()
+  const writter = transformStream.writable.getWriter()
+  const encoder = new TextEncoder()
+
+  return new Response(transformStream.readable, {
+    headers: {
+      'Content-Type': 'text/event-stream',
+      Connection: 'keep-alive',
+      'Cache-Control': 'no-cache, no-transform',
+    },
+    status: 200,
+  })
 }
