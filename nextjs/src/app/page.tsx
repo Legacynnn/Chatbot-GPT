980aa3cb2c1d2867eccb36ed76639bb9716e6c67
Legacynnn <danmelomour@gmail.com>
2023-04-23 21:07:18 -0300

    🔧: create a new chat and create a message

--- nextjs/src/app/page.tsx
+++ nextjs/src/app/page.tsx
@@ -1,9 +1,10 @@
 'use client'
 
 import useSWR from 'swr'
-import { fetcher } from '@/http/http'
+import ClientHttp, { fetcher } from '@/http/http'
 import { Chat, Message } from '@prisma/client'
-import { useRouter } from 'next/navigation'
+import { useRouter, useSearchParams } from 'next/navigation'
+import { FormEvent, useEffect, useState } from 'react'
 
 type ChatWithFirstMessage = Chat & {
   message: [Message]
@@ -11,17 +12,71 @@ type ChatWithFirstMessage = Chat & {
 
 export default function Home() {
   const router = useRouter()
-  const { data: chats } = useSWR<ChatWithFirstMessage[]>('chats', fetcher, {
-    fallbackData: [],
-  })
+  const searchParams = useSearchParams()
+  const chatIdParams = searchParams.get('id')
+  const [chatId, setChatId] = useState(chatIdParams)
 
-  const { data: messages } = useSWR<ChatWithFirstMessage[]>(
-    `chats//messages`,
+  const { data: chats, mutate: mutateChats } = useSWR<ChatWithFirstMessage[]>(
+    'chats',
+    fetcher,
+    {
+      fallbackData: [],
+      revalidateOnFocus: false,
+    },
+  )
+
+  const { data: messages, mutate: mutateMessages } = useSWR<Message[]>(
+    chatIdParams ? `chats/${chatIdParams}/messages` : null,
     fetcher,
     {
       fallbackData: [],
     },
   )
+
+  useEffect(() => {
+    setChatId(chatIdParams)
+  }, [chatIdParams])
+
+  useEffect(() => {
+    const textArea = document.querySelector('#message') as HTMLTextAreaElement
+    textArea.addEventListener('keydown', (event) => {
+      if (event.key === 'Enter' && !event.shiftKey) {
+        event.preventDefault()
+      }
+    })
+    textArea.addEventListener('keyup', (event) => {
+      if (event.key === 'Enter' && !event.shiftKey) {
+        const form = document.querySelector('#form') as HTMLFormElement
+        const submitButton = form.querySelector('button') as HTMLButtonElement
+        form.requestSubmit(submitButton)
+      }
+    })
+  }, [])
+
+  async function onSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    const textArea = event.currentTarget.querySelector(
+      'textarea',
+    ) as HTMLTextAreaElement
+    const message = textArea.value
+
+    if (!chatId) {
+      const newChat: ChatWithFirstMessage = await ClientHttp.post(
+        'chats',
+        message,
+      )
+      mutateChats([newChat, ...chats!])
+      setChatId(newChat.id)
+    } else {
+      const newMessage: Message = await ClientHttp.post(
+        `chats/${chatId}/messages`,
+        { message },
+      )
+      mutateMessages([...messages!, newMessage], false)
+    }
+    textArea.value = ''
+  }
 
   return (
     <div className="flex gap-5">
@@ -31,7 +86,7 @@ export default function Home() {
         <ul>
           {chats!.map((chat, key) => (
             <li key={key} onClick={() => router.push(`/?id=${chat.id}`)}>
-              {chat.message[0].content}
+              {chat!.message[0].content}
             </li>
           ))}
         </ul>
@@ -40,10 +95,12 @@ export default function Home() {
       <div>
         Centro
         <ul>
-          <li>Mensagens</li>
+          {messages?.map((message, key) => {
+            return <li key={message.id}>{message.content}</li>
+          })}
         </ul>
-        <form action="">
-          <textarea placeholder="Digite sua pergunta"></textarea>
+        <form id="form" onSubmit={onSubmit}>
+          <textarea id="message" placeholder="Digite sua pergunta"></textarea>
         </form>
       </div>
     </div>
